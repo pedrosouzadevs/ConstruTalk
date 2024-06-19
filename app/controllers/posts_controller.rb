3ab1eba71279694c77267b4c5bfad553e2a568d2
Pedro Souza <phmartinsdesouza@gmail.com>
2024-06-19 16:40:26 -0300

    controllers actions and routes are done until now

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,57 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def my_posts
+    @posts = Post.where(user_id: current_user.id)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to @post
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+    @user = current_user
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit',  status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to posts_path
+    else
+      render 'show', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
-  validate :content, presence: true
-  validate :user_id, presence: true
+  validates :content, presence: true
+  validates :user_id, presence: true
 end
